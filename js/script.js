1a4c1766777ecb1afebdb0a41469c16d62fb95f0
anthony_f <afanous@hotmail.com>
2023-07-03 16:37:48 -0400

    Added ability to click and view specific repo info

--- js/script.js
+++ js/script.js
@@ -2,7 +2,16 @@
 const overview = document.querySelector(".overview");
 
 // Repo list class selector
-const repoList = document.querySelector(".repo-list")
+const repoList = document.querySelector(".repo-list");
+
+// Repos section class selector
+const reposSection = document.querySelector(".repos");
+
+// Repo data class selector
+const repoDataSection = document.querySelector(".repo-data")
+
+// Return button class selector
+const returnButton = document.querySelector(".view-repos")
 
 // Github username
 const username = "Tony2450";
@@ -49,11 +58,54 @@ const userRepoListFetch = async function () {
 };
 
 // Display fetched repo list
-const displayRepos = function(repoListJson) {
-    repoListJson.forEach(function(repo,index,array){
+const displayRepos = function (repoListJson) {
+    repoListJson.forEach(function (repo, index, array) {
         let listDiv = document.createElement("li");
         listDiv.classList.add("repo");
         listDiv.innerHTML = `<h3>${repo.name}</h3>`
         repoList.append(listDiv);
     })
+};
+
+// Listen for click event on unordered list
+repoList.addEventListener("click", function (e) {
+    if (e.target.matches("h3")) {
+        const repoName = e.target.innerText;
+        repoInfoFetch(repoName);
+    }
+});
+
+// Async function for specific repo info
+const repoInfoFetch = async function (repoName) {
+    const repoInfoData = await fetch(`https://api.github.com/repos/${username}/${repoName}`);
+    const repoInfo = await repoInfoData.json();
+    const fetchLanguages = await fetch(`https://api.github.com/repos/${username}/${repoName}/languages`);
+    const languageData = await fetchLanguages.json();
+    let languages = [];
+    for (let language in languageData) {
+        languages.push(language);
+    };
+    displayRepoInfo(repoInfo, languages);
+};
+
+// Display specific repo information
+const displayRepoInfo = function (repoInfo, languages) {
+    repoDataSection.innerHTML = "";
+    const repoDataDiv = document.createElement("div");
+    repoDataDiv.innerHTML = `<h3>Name: ${repoInfo.name}</h3>
+    <p>Description: ${repoInfo.description}</p>
+    <p>Default Branch: ${repoInfo.default_branch}</p>
+    <p>Languages: ${languages.join(", ")}</p>
+    <a class="visit" href="${repoInfo.clone_url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>`;
+    repoDataSection.classList.remove("hide");
+    repoDataSection.append(repoDataDiv);
+    reposSection.classList.add("hide");
+    returnButton.classList.remove("hide");
 }
+
+// Listen for click event on return button
+returnButton.addEventListener("click", function(){
+    repoDataSection.classList.add("hide");
+    reposSection.classList.remove("hide");
+    returnButton.classList.add("hide");
+})
