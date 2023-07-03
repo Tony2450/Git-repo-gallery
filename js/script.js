241d02264e26d74af4a81c97ed9b851b2d9a004b
anthony_f <afanous@hotmail.com>
2023-07-03 13:14:25 -0400

    Added display of github user info

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// Overview class selector
+const overview = document.querySelector(".overview");
+
+// Github username
+const username = "Tony2450";
+
+// Async fetch function to call github API for profile info
+const userInfoFetch = async function () {
+    const userData = await fetch(`https://api.github.com/users/${username}`);
+    const userJsonData = await userData.json();
+    console.log(userJsonData);
+    displayInfo(userJsonData);
+};
+
+userInfoFetch();
+
+// Display fetched user information
+const displayInfo = function (userJsonData) {
+    const userInfoDiv = document.createElement("div");
+    userInfoDiv.classList.add("user-info");
+    let avatar = userJsonData.avatar_url;
+    let name = userJsonData.name;
+    let bio = userJsonData.bio;
+    let location = userJsonData.location;
+    let publicRepoCount = userJsonData.public_repos;
+    userInfoDiv.innerHTML = `
+    <figure>
+      <img alt="user avatar" src=${avatar} />
+    </figure>
+    <div>
+      <p><strong>Name:</strong> ${name}</p>
+      <p><strong>Bio:</strong> ${bio}</p>
+      <p><strong>Location:</strong> ${location}</p>
+      <p><strong>Number of public repos:</strong> ${publicRepoCount}</p>
+    </div> `;
+    overview.append(userInfoDiv);
+};
